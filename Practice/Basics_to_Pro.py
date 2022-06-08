090e5eaf3e3970e6e8e405d246811d5a2798aaea
Alec <60586484+azheltov@users.noreply.github.com>
2022-06-07 20:53:14 -0400

    Create Basics_to_Pro.py

---
+++ Practice/Basics_to_Pro.py
@@ -0,0 +1,14 @@
+for i in range(29, 3, -1):
+    print(i, end=" ")
+    
+
+    
+    
+    
+    
+    
+from math import sqrt	
+L = []	
+for i in range(1, 100):	
+	L.append(round(sqrt(i), 4))
+print(L)	

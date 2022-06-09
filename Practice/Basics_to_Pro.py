ceabbe2ffedcc90c6b807786bcc0e128b88b4395
Alec <60586484+azheltov@users.noreply.github.com>
2022-06-08 20:26:19 -0400

    Update Basics_to_Pro.py

--- Practice/Basics_to_Pro.py
+++ Practice/Basics_to_Pro.py
@@ -12,3 +12,17 @@ L = []
 for i in range(1, 100):	
 	L.append(round(sqrt(i), 4))
 print(L)	
+
+
+
+# AND, OR, NOT
+
+num = eval(input ("Please enter a number: "))                                 # eval - convert to int
+if not (num == 0 or num ==2 or num==5 or 10<num<15 or 20<num<25):
+	print("Ok")
+else:
+	print("Not okay")
+	
+	
+	
+	

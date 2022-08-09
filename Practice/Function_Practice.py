b26ae64d4afb953430aabb48ca4aac6ebc1fecfc
Alec <60586484+azheltov@users.noreply.github.com>
2022-08-08 21:05:08 -0400

    Update Function_Practice.py

--- Practice/Function_Practice.py
+++ Practice/Function_Practice.py
@@ -1,1 +1,23 @@
+def string_reverse(s):                                             
+  i = len(s)
+  reverse = ""
+  while i > 0:
+    reverse += S[i-1]                                                      #reverse String
+    i -= 1
+  return reverse
 
+s = "string"
+print(sting_reverse('string'))
+    
+###
+
+from random import choice
+
+def rand_lower_string(n):
+  n = int(input("Please enter an int: "))
+  s = ""
+  for i in range(n):
+    s += choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+  return s
+
+###

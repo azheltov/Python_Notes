136364a28eba581b60b62b22ed631837d610938b
Alex Zheltov <zheltov.alex@gmail.com>
2025-04-23 13:57:12 -0400

    Added file_translator.py – file-to-Japanese translator with error handling

---
+++ file_translator.py
@@ -0,0 +1,11 @@
+from translate import Translator
+
+translator = Translator(to_lang="ja")
+
+try:
+    with open('./test.txt', mode='r') as my_file:
+        text = my_file.read()
+        translation = translator.translate(text)
+        print(translation)
+except FileNotFoundError as e:
+    print("Check your file path, silly!")
